6698361afb2b3815f8fedeeb7a09174ae180e58c
Тимофей Булгаков <ttbulgakov@edu.hse.ru>
2025-02-13 14:19:06 +0300

    add README

--- Zoo/obj/Debug/net9.0/Zoo.AssemblyInfo.cs
+++ Zoo/obj/Debug/net9.0/Zoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa9410d99e809ab52e69923e0833ec5cd9de2c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
